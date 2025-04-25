4964c9b91a0000b46cb369307e0f78db1c4640b6
Kan Yilmaz <kan.yilmaz.me@gmail.com>
2025-04-25 19:55:07 +0200

    add reading time

---
+++ _plugins/reading_time_filter.rb
@@ -0,0 +1,17 @@
+module Jekyll
+  module ReadingTimeFilter
+    def reading_time(input)
+      # Remove front matter
+      text = input.to_s.gsub(/---\s*[\s\S]*?---\s*/, '')
+      
+      # Simple word count
+      words = text.split(/\s+/).reject(&:empty?)
+      minutes = (words.size / 200.0).ceil # 200 words per minute
+      minutes = 1 if minutes < 1 # Minimum 1 minute
+      
+      "#{minutes} min read"
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::ReadingTimeFilter) 
